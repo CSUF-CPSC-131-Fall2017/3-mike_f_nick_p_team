fb1b82951e348bf30ef12342eed585c851a5a93a
NPosey97 <31231207+NPosey97@users.noreply.github.com>
2017-10-07 17:15:27 -0700

    Update Webpage.h

--- Webpage.h
+++ Webpage.h
@@ -5,13 +5,14 @@ using namespace std;
 
 class Webpage {
 public:
-    Webpage();
-    Webpage(const string& webpageURL, const time_t& timeVisited);
-    string getURL();
-    time_t getTime();
+	Webpage();
+	Webpage(const string& webpageURL, const time_t& timeVisited);
+	string getURL();
+	time_t getTime();
 
 private:
-    // Add private member variables for your class along with any
-    // other variables required to implement the public member functions
-    // TO BE COMPLETED
+	// Add private member variables for your class along with any
+	// other variables required to implement the public member functions
+	time_t time;
+	string url;
 };
