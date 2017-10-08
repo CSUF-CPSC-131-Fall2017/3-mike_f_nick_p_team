328cbd1121237abeb25b3370cf5dac93ce43d5d7
NPosey97 <31231207+NPosey97@users.noreply.github.com>
2017-10-07 18:50:10 -0700

    Update BrowserHistory.h

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -8,26 +8,36 @@
 #include "Webpage.h"
 using namespace std;
 
+
+
+//In this case, BrowserHistory is the DLinkedList class
 class BrowserHistory {
 public:
-    BrowserHistory();
-    ~BrowserHistory();
+	BrowserHistory();
+	~BrowserHistory();
 
-    void visitSite(Webpage newSite);
-    string back();
-    string forward();
+	void visitSite(Webpage newSite);
+	string back();
+	string forward();
 
-    void readHistory(string fileName);
+	void readHistory(string fileName);
 
-    string getURL();
-    size_t getNavSize();
+	string getURL();
+	size_t getNavSize();
 
-    void printBackSites();
-    void printForwardSites();
-    void printFullHistory();
+	void printBackSites();
+	void printForwardSites();
+	void printFullHistory();
 
 private:
-    // Add private member variables for your class along with any
-    // other variables required to implement the public member functions
-    // TO BE COMPLETED
+	// Add private member variables for your class along with any
+	// other variables required to implement the public member functions
+	// TO BE COMPLETED
+	int numVisited;
+	Webpage *head;
+	Webpage *tail;
+
+	//cursor will be used with the navigation functions of back and forward to keep track of the current location
+	//LOOK AT CURSOR WITH REGARDS TO NECESSARY FUNCTIONALITY NEEDED
+	Webpage *cursor;
 };
