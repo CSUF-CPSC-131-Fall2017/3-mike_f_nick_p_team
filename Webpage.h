ae7040b8d7f4760d9339dad474d018514c91dda5
NPosey97 <31231207+NPosey97@users.noreply.github.com>
2017-10-07 18:48:50 -0700

    Update Webpage.h

--- Webpage.h
+++ Webpage.h
@@ -13,6 +13,13 @@ public:
 private:
 	// Add private member variables for your class along with any
 	// other variables required to implement the public member functions
+	// TO BE COMPLETED
 	time_t time;
 	string url;
+
+	// adds functionality of webpage as a node
+	Webpage* next;
+	Webpage* prev;
+	friend class BrowserHistory;
 };
+
