200d45f7596bae425da6560f59eadb954048ed23
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2017-10-10 14:53:35 -0700

    Update Webpage.h

--- Webpage.h
+++ Webpage.h
@@ -7,10 +7,15 @@ class Webpage {
 public:
     Webpage();
     Webpage(const string& webpageURL, const time_t& timeVisited);
+    Webpage(&Webpage);
+    operator=(&Webpage);
     string getURL();
     time_t getTime();
 
 private:
    string url;
    time_t time;
+   Webpage* next;
+   Webpage* prev;
+   friend class BrowserHistory;
 };
