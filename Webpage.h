cd3f481f4756035c04b563e9ffe25f1ad9a32af4
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2017-10-13 12:27:33 -0700

    Update Webpage.h

--- Webpage.h
+++ Webpage.h
@@ -5,17 +5,16 @@ using namespace std;
 
 class Webpage {
 public:
-    Webpage();
-    Webpage(const string& webpageURL, const time_t& timeVisited);
-    Webpage(&Webpage);
-    operator=(&Webpage);
-    string getURL();
-    time_t getTime();
+	Webpage();
+	Webpage(const string& webpageURL, const time_t& timeVisited);
+	string getURL();
+	time_t getTime();
+	void setInfo(const Webpage&);
 
 private:
-   string url;
-   time_t time;
-   Webpage* next;
-   Webpage* prev;
-   friend class BrowserHistory;
+	time_t time;
+	string url;
+	Webpage* next;	//Converts Webpage into a doubly linked list node class.
+	Webpage* prev;
+	friend class BrowserHistory;
 };
