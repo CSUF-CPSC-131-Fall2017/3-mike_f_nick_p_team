318cf007f93c2fa0bebddc7054ae23117eef033d
NPosey97 <31231207+NPosey97@users.noreply.github.com>
2017-10-14 19:48:40 -0700

    Update BrowserHistory.cpp

--- BrowserHistory.cpp
+++ BrowserHistory.cpp
@@ -189,7 +189,7 @@ void BrowserHistory::readHistory(string fileName) {
 				//new site is visited if first character of string in n as in New
 				//only then will url and time show up on the txt file, else they won't exist on the line
 				
-				//is this breaking it?
+				//this is breaking the program
 				myFile >> url >> timeVisited;
 				
 				cout << url;
