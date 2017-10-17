1c27d524c71105e16ed5bb1821237e668b1d0585
NPosey97 <31231207+NPosey97@users.noreply.github.com>
2017-10-16 21:53:57 -0700

    Update BrowserHistory.h

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -9,39 +9,25 @@
 using namespace std;
 
 
-
-//In this case, BrowserHistory is the DLinkedList class
-class BrowserHistory {
+//BrowserHistory acts as a doubly linked list class.
+class BrowserHistory { 
 public:
 	BrowserHistory();
 	~BrowserHistory();
-
 	void visitSite(Webpage newSite);
 	string back();
 	string forward();
-
-	void readHistory(string fileName);
-
+	void readHistory(string filename);
 	string getURL();
 	size_t getNavSize();
-
 	void printBackSites();
 	void printForwardSites();
 	void printFullHistory();
 
 private:
-	// Add private member variables for your class along with any
-	// other variables required to implement the public member functions
-	// TO BE COMPLETED
 	int numVisited;
 	Webpage *head;
 	Webpage *tail;
-	//this list stores the full UNALTERED browser history based solely on time accessed
-	list<Webpage> fullHistory;
-	// full history will not delete elements, only add and does not need a cursor to change
-	
-
-	//cursor will be used with the navigation functions of back and forward to keep track of the current location
-	//LOOK AT CURSOR WITH REGARDS TO NECESSARY FUNCTIONALITY NEEDED
-	Webpage *cursor;
+	list<Webpage> fullHistory; //Will hold every website visited ordered by time visited. Will never need to remove elements.
+	Webpage *cursor; //Used to keep track of location within the browsing history list.
 };
